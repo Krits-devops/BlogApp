a76db467204be13efbf704a9b662428847cd6224
Krits-devops <kritagyarm9@gmail.com>
2023-11-14 18:56:47 -0500

    Created Mapping for Comment and Post

--- routes/blog.js
+++ routes/blog.js
@@ -4,15 +4,16 @@ const router = express.Router();
 
 
 // Import Controller
-
+const {createComment} = require("../controllers/commentController");
+const { createPost, getAllPosts } = require("../controllers/postController");
+const { likes } = require("../controllers/likeController");
 
 // Mapping Creation
 
-
-
-
-
-
+router.post("/comment/create", createComment);
+router.post("/posts/create", createPost);
+router.get("/posts", getAllPosts);
+router.post("/likes", likes);
 
 // Exporting
 
