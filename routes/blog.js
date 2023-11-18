4d620bd6091396cccfeea174e61a1f0b70a29182
Krits-devops <kritagyarm9@gmail.com>
2023-11-18 00:30:05 -0500

    Rectified the issue of route creation

--- routes/blog.js
+++ routes/blog.js
@@ -6,14 +6,16 @@ const router = express.Router();
 // Import Controller
 const {createComment} = require("../controllers/commentController");
 const { createPost, getAllPosts } = require("../controllers/postController");
-const { likes } = require("../controllers/likeController");
+const { likePost, unlikePost } = require("../controllers/likeController");
 
 // Mapping Creation
 
 router.post("/comment/create", createComment);
 router.post("/posts/create", createPost);
 router.get("/posts", getAllPosts);
-router.post("/likes", likes);
+router.post("/likes/like", likePost);
+router.post("/likes/unlikes", unlikePost);
+
 
 // Exporting
 
